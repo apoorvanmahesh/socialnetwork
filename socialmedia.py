9953a0388cb659613094de05d0db741110e72e08
maddie-anderson <maddie_anderson@comcast.net>
2017-07-27 15:00:10 -0500

    Update socialmedia.py
    Updated with connections

--- socialmedia.py
+++ socialmedia.py
@@ -2,8 +2,15 @@
 usernameArray=[]
 addName = True
 class User():
+
     def __init__(self,name):
+        self.connections=[]
         self.name = name
+
+    def addConnection(self,invite):
+        self.connections.append(invite)
+        print("Your connections: %s." % self.connections)
+
 A = User("Vee")
 B = User("Maddie")
 C = User("Nisha")
@@ -14,13 +21,14 @@ usernameArray.append(C)
 usernameArray.append(D)
 
 
+
 while True:
     addName = True
-    user_input = input("What is your username?")
+    user_input = input("What username do you want?")
     x = User(user_input)
+
     #print(usernameArray[0].name)
     for i in range (len(usernameArray)):
-        print(usernameArray[i].name)
         if user_input == usernameArray[i].name:
             addName = False
             print("Sorry, your username has already been taken. Please choose another one.")
@@ -28,11 +36,23 @@ while True:
     if addName == True:
         usernameArray.append(x)
 
-        #print(usernameArray[i].name)
+    print(" ")
+    print(" ")
 
-    # if user_input not in username:
-    #     username.append(x)
-    # elif user_input in username:
-    #     print("Sorry, your username has already been taken. Please choose another one.")
-    # print(username)
-    # print(username[0].name)
+    for i in range (len(usernameArray)):
+        print(usernameArray[i].name)
+    print("You can connect with people")
+
+    yourName = input("What is your username?")
+    index = None
+    userFound = False
+    for i in range (len(usernameArray)):
+        if yourName == usernameArray[i].name:
+            index = i;
+            userFound = True
+            break
+    if userFound == False:
+        print("Sorry, the username does not exist")
+    else:
+        inviteName = input("Who do you want to connect with?")
+        usernameArray[index].addConnection(inviteName)
